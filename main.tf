9c21b51b8c6090a68ae2393a3b6437b9b3eef6cf
kiranReddyBokkala <kiran@gmail.com>
2025-01-27 18:10:23 +0000

    script related to VPC, subnets, Security Groups, Internet Gateways, Routes This is safe to push the keys, nothing important and all the privacy measures have been taken

---
+++ main.tf
@@ -0,0 +1,132 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA6ODU3AVWRAICEGGM"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MyVPCforTerraform"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet1"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet2"
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet3"
+  }  
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private_subnet1"
+  }  
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet2"
+  }  
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "private_subnet3"
+  }
+
+}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Terraform_IG"
+  }  
+}
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "RouteTable_terraform"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+resource "aws_route_table_association" "public_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_association_3" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_security_group" "http_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "SG_Terraform"
+  }
+}
+
